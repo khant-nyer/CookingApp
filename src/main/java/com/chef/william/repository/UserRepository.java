5250dad6a93afb3ad9aebc60337c72ef2d27390e
khant-nyer <58938149+khant-nyer@users.noreply.github.com>
2026-03-11 11:49:20 +0700

    Implement Cognito registration phases 1-3

--- src/main/java/com/chef/william/repository/UserRepository.java
+++ src/main/java/com/chef/william/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.chef.william.repository;
 import com.chef.william.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByCognitoSub(String cognitoSub);
 }
