9e6fff89b53f19d61b0949e126f389b5352dd579
User <weixing.khant@gmail.com>
2026-03-12 16:45:22 +0700

    import delegatingoauth2 in securityConfig, update congnito config in app properties

--- src/main/java/com/chef/william/config/security/SecurityConfig.java
+++ src/main/java/com/chef/william/config/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
